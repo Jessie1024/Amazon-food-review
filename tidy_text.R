eb73d8d650a01de610a89e7bb5043a7e81d0b6a9
Jessie1024 <jessy1024qh@gmail.com>
2020-09-06 02:46:16 -0400

    update tidytext rscript

---
+++ tidy_text.R
@@ -0,0 +1,45 @@
+#######Text Mining################
+
+######tidy the summary and text#######
+
+library(stringr)
+library(tidytext)
+
+#####tidy the summary and text#######
+raw_data<-read_csv("derived_data/tidy_data.csv")
+raw_data$summary<-raw_data$summary%>%
+  tolower() %>%
+  str_replace_all (",", " ") %>%
+  str_replace_all ("-", " ") %>%
+  str_replace_all ("&", " ") %>%
+  str_replace_all ("!", " ") 
+
+raw_data$text<-raw_data$text%>%
+  tolower() %>%
+  str_replace_all (",", " ") %>%
+  str_replace_all ("-", " ") %>%
+  str_replace_all ("&", " ") %>%
+  str_replace_all ("!", " ") 
+
+####expand the contractions##########
+fix.contractions<-function(doc){
+  doc<-gsub("won't", "will not", doc)
+  doc<-gsub("can't", "can not", doc)
+  doc<-gsub("n't", " not", doc)
+  doc<-gsub("'ll", " will", doc)
+  doc<-gsub("'re", " are", doc)
+  doc<-gsub("'ve", " have", doc)
+  doc<-gsub("'m", " am", doc)
+  doc<-gsub("'d", "would", doc)
+  
+  return(doc)
+}
+
+raw_data$summary<-sapply(raw_data$summary,fix.contractions)
+raw_data$text<-sapply(raw_data$text, fix.contractions)
+
+write_csv(raw_data,"derived_data/clean_data.csv")
+summary(raw_data)
+
+####change to data format######
+class(raw_data$time) <- c('POSIXt','POSIXct')
