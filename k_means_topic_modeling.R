6a450c29aa8ab6b11e431f83dc37593d94cba494
Jessie1024 <jessy1024qh@gmail.com>
2020-10-07 14:36:01 -0400

    split from the topic modeling rscript

---
+++ k_means_topic_modeling.R
@@ -0,0 +1,101 @@
+library(tidytext) 
+library(topicmodels) 
+library(tidyr) 
+library(dplyr) 
+library(ggplot2) 
+library(kableExtra) 
+library(knitr) 
+library(gridExtra)
+library(formattable) 
+library(tm) 
+library(plotly) 
+library(tidyverse)
+library(quanteda)
+library(reshape2)
+library(stringr)
+my_colors <- c("#66C2A5", "#FC8D62", "#8DA0C", "#E78AC3", "#A6D854", "#FFD92F")
+
+#customize ggplot2's default theme settings
+#this tutorial doesn't actually pass any parameters, but you may use it again in future tutorials so it's nice to have the options
+theme_lyrics <- function(aticks = element_blank(),
+                         pgminor = element_blank(),
+                         lt = element_blank(),
+                         lp = "none")
+{
+  theme(plot.title = element_text(hjust = 0.5), #center the title
+        axis.ticks = aticks, #set axis ticks to on or off
+        panel.grid.minor = pgminor, #turn on or off the minor grid lines
+        legend.title = lt, #turn on or off the legend title
+        legend.position = lp) #turn on or off the legend
+}
+
+#customize the text tables for consistency using HTML formatting
+my_kable_styling <- function(dat, caption) {
+  kable(dat, "html", escape = FALSE, caption = caption) %>%
+    kable_styling(bootstrap_options = c("striped", "condensed", "bordered"),
+                  full_width = FALSE)
+}
+
+word_chart <- function(data, input, title) {
+  data %>%
+    #set y = 1 to just plot one variable and use word as the label
+    ggplot(aes(as.factor(row), 1, label = input, fill = factor(topic) )) +
+    #you want the words, not the points
+    geom_point(color = "transparent") +
+    #make sure the labels don't overlap
+    geom_label_repel(nudge_x = .2,  
+                     direction = "y",
+                     box.padding = 0.1,
+                     segment.color = "transparent",
+                     size = 3) +
+    facet_grid(~topic) +
+    theme_lyrics() +
+    theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
+          #axis.title.x = element_text(size = 9),
+          panel.grid = element_blank(), panel.background = element_blank(),
+          panel.border = element_rect("lightgray", fill = NA),
+          strip.text.x = element_text(size = 9)) +
+    labs(x = NULL, y = NULL, title = title) +
+    #xlab(NULL) + ylab(NULL) +
+    #ggtitle(title) +
+    coord_flip()
+}
+source_dtm<-readRDS("derived_data/source_dtm.rds")
+source_tidy<-readRDS("derived_data/source_tidy.rds")
+#k-means(takes 10 min)
+
+
+set.seed(123)
+k<-5
+kmeansResult<-kmeans(source_dtm,k)
+str(kmeansResult)
+
+#k-means themes with Top words
+
+num_words<-8
+kmeans_topics <- lapply(1:k, function(i) {
+  s <- sort(kmeansResult$centers[i, ], decreasing = T)
+  names(s)[1:num_words]
+})
+
+#make sure it's a data frame
+kmeans_topics_df <- as.data.frame(kmeans_topics)
+#label the topics with the word Topic
+names(kmeans_topics_df) <- paste("Topic", seq(1:k), sep = " ")
+#create a sequential row id to use with gather()
+kmeans_topics_df <- cbind(id = rownames(kmeans_topics_df),
+                          kmeans_topics_df)
+#transpose it into the format required for word_chart()
+kmeans_top_terms <- kmeans_topics_df %>% gather("topic", "term", `Topic 1`,
+                                                `Topic 2`,`Topic 3`,`Topic 4`,
+                                                `Topic 5`)
+
+kmeans_top_terms <- kmeans_top_terms %>%
+  group_by(topic) %>%
+  mutate(row = row_number()) %>% #needed by word_chart()
+  ungroup()
+
+title <- paste("K-Means Top Terms for", k, "Topics")
+word_chart(kmeans_top_terms, kmeans_top_terms$term, title)+
+  ggsave(filename = "image/k_means_for_5_topics.png")
+
