72d69b8c6b977c37e7e7e1a5128f04509b7b0243
Jessie1024 <jessy1024qh@gmail.com>
2020-10-07 15:40:49 -0400

    Put the overall score into one plot

--- Overall.R
+++ Overall.R
@@ -1,19 +1,39 @@
 #####overall score distribution#############
-library(tidyverse);
+library(tidyverse)
+library(gridExtra)
 raw_data<-read_csv("derived_data/tidy_data.csv")
 overall_hist<-ggplot(raw_data, aes(x=raw_data$score))+
   geom_histogram(binwidth=1,fill="#69b3a2", color="#e9ecef", alpha=0.9)+
-  ggtitle("Histogram of Amazon food review score distribution")+
-  xlab("Score")
-ggsave("image/Overall_score_distribution.png", plot = overall_hist)
+  xlab("Score")+
+ ggsave("image/Overall_score_distribution.png")
 
 #####overall helpfulness##############
 help_hist<-ggplot(raw_data, aes(x=raw_data$helpfulnessnumerator))+
-  geom_histogram(color="black",fill="white", alpha=0.5)+
+  geom_histogram(binwidth=1,fill="darkolivegreen1",alpha=0.8)+
   xlim(0,20)+
-  stat_bin(bins=30)+
   ylim(0,1.5e+05)+
-  ggtitle("Histogram of Amazom foor review helpfullness distribution")+
-  xlab("Helpfulness of the review")
-ggsave("image/Helpfulness of the review.png", plot=help_hist)
-##This does not make sense.....#######
+  xlab("Helpfulness")+
+  ggsave("image/Helpfulness_review.png")
+
+#combine the two
+
+overall<-grid.arrange(
+  overall_hist,
+  help_hist,
+  nrow=1,
+  top="Amazon food review score and helpfulness"
+)
+ggsave(overall,file="image/overall_amazon_food_socre_helpfullness.png")
+
+  saveRDS(overall,file= "image/overall_amazon_food_socre_helpfullness.rds")
+
+
+
+
+
+
+
+
+
+
+
