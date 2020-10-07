08336053f4da76878d7b637e82a237dd980272ac
Jessie1024 <jessy1024qh@gmail.com>
2020-10-07 15:44:03 -0400

    put the histograms into one plot

--- length_rank.R
+++ length_rank.R
@@ -16,6 +16,7 @@ full_word_count<-raw_data %>%
   arrange(desc(num_words))
 
 full_word_count<-write_csv(full_word_count,"derived_data/full_word_count.csv")
+full_word_count<-read_csv("derived_data/full_word_count.csv")
 #####top20 graph#####3
   full_word_count[1:23,] %>%
   ungroup(num_words, id, summary,score) %>%
@@ -26,7 +27,7 @@ full_word_count<-write_csv(full_word_count,"derived_data/full_word_count.csv")
   kable_styling(bootstrap_options = 
                   c("striped", "condensed", "bordered"), 
                 full_width = FALSE)%>%
-  save_kable("image/top20 length review.png")
+  save_kable("image/top20_length_review.png")
   
   #######least20 graph#######
   
@@ -39,7 +40,7 @@ full_word_count<-write_csv(full_word_count,"derived_data/full_word_count.csv")
     kable_styling(bootstrap_options = 
                     c("striped", "condensed", "bordered"), 
                   full_width = FALSE)%>%
-    save_kable("image/last20 length review.png")
+    save_kable("image/last20_length_review.png")
   
   
   
